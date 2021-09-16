5e3a5224a7791efcc0c250bd06e0242eb2488032
Viktor Crow <varjuviktor69@gmail.com>
2021-09-16 12:29:08 +0200

    Name class created, Status enum created

---
+++ Spectator/Spectator/Model/Name.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spectator.Model
+{
+    /// <summary>
+    /// Different statuses of the card
+    /// </summary>
+    enum Status
+    {
+        Active,
+        Inactive,
+        Expired
+    }
+    /// <summary>
+    /// Name class
+    /// </summary>
+    class Name
+    {
+
+
+    }
+}
