c07d1de0e69a1d80e0ff6c1b2e2c650851609c63
Viktor Crow <varjuviktor69@gmail.com>
2021-09-20 12:14:41 +0200

    Name class finished

--- Spectator/Spectator/Model/Name.cs
+++ Spectator/Spectator/Model/Name.cs
@@ -8,5 +8,25 @@ namespace Spectator.Model
     {
         string firstName, lastName;
 
+        public Name(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+        /// <summary>
+        /// Properties of Name class
+        /// </summary>
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FullName
+        {
+            get
+            { return firstName + " " + lastName; }
+        }
+        public override string ToString()
+        {
+            return FullName;
+        }
+
     }
 }
