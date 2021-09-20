a0f450122015477e06581ccf4f9c6071a8347de9
sz0varzol <sz0varzol@vasvari.org>
2021-09-20 11:32:02 +0200

    Re upload

--- Spectator/Spectator/Model/CreditCard.cs
+++ Spectator/Spectator/Model/CreditCard.cs
@@ -3,57 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Spectator.Model
-{ 
- 
+{
+
     class CreditCard
     {
         private DateTime expireDate;
         private string cardNumber;
-        /// <summary>
-        /// -5
-        /// </summary>
-        /// <param name="expireDate"></param>
-        /// <param name="cardNumber"></param>
-        public CreditCard(string cardNumber, DateTime expireDate)
-        {
-            this.ExpireDate = expireDate;
-            this.CardNumber = cardNumber;
-        }
+     
+       
 
-      
-        public DateTime ExpireDate { get => expireDate; set => expireDate = value; }
-    public string CardNumber { get => cardNumber; set => cardNumber = value; }
 
-    public bool Expired
-    {
-        get
-        {
-            /// checking for expiration date 
 
-            if (expireDate < DateTime.Now)
-            {
-                return true;
-            }
-            else return false;
-        }
+
     }
-    public override string ToString()
-    {
-        string result = "Hitelkártya : " + cardNumber;
-        result += "\nLejarati datum " + expireDate;
-        if (Expired)
-        {
-            result += "Kartya lejart";
-
-        }
-        else
-        {
-            result += "A kártya aktív";
-
-        }
-        return result;
-    }
-
-
-
 }
